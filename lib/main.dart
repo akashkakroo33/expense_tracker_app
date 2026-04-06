f0053ab448b1dcfbf525bb2b65f7a2e438e96f7a
Akash kakroo <akashkakroo33@example.com>
2026-04-06 16:34:34 +0530

    Static app completed

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
@@ -24,7 +21,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   List<Map<String, dynamic>> expenses = [];
 
   void addExpense() {
@@ -32,7 +28,88 @@ class _HomePageState extends State<HomePage> {
     TextEditingController categoryController = TextEditingController();
     DateTime selectedDate = DateTime.now();
 
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Add Expenses"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: categoryController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: "Category",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: amountController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Amount",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("${selectedDate.toLocal()}".split(' ')[0]),
+                  TextButton(
+                    onPressed: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: selectedDate,
+                        firstDate: DateTime(2020),
+                        lastDate: DateTime(2100),
+                      );
 
+                      if (pickedDate != null) {
+                        setState(() {
+                          selectedDate = pickedDate;
+                        });
+                      }
+                    },
+                    child: Text("Select Date"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                if (amountController.text.isEmpty ||
+                    categoryController.text.isEmpty) {
+                  return;
+                }
+
+                setState(() {
+                  expenses.add({
+                    "amount": amountController.text,
+                    "category": categoryController.text,
+                    "date": selectedDate,
+                  });
+                });
+
+                Navigator.pop(context);
+              },
+              child: Text("Save"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -44,9 +121,7 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.amber,
-        actions: [
-          IconButton(icon: Icon(Icons.settings), onPressed: () {}),
-        ],
+        actions: [IconButton(icon: Icon(Icons.settings), onPressed: () {})],
       ),
 
       floatingActionButton: FloatingActionButton(
@@ -54,6 +129,21 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.amberAccent,
         child: Icon(Icons.add),
       ),
+
+      body: expenses.isEmpty
+          ? Center(child: Text("No expenses added so far"))
+          : ListView.builder(
+              itemCount: expenses.length,
+              itemBuilder: (context, index) {
+                final expense = expenses[index];
+                return ListTile(
+                  leading: Icon(Icons.control_point_duplicate),
+                  title: Text(expense["category"]),
+                  subtitle: Text("₹${expense["amount"]}"),
+                  trailing: Text("${expense["date"].toLocal()}".split(' ')[0]),
+                );
+              },
+            ),
     );
   }
 }
